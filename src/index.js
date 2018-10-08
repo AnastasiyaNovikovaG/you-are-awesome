ca47e0952359672e46c2e34c91cabe16a609980b
nasti <AnastasiyaNovikovaG>
2018-10-08 19:42:41 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (property) => {
+    return property;
+};
+const createNotEnumerableProperty = (property) => {
+    return Symbol(property);
+};
+const createProtoMagicObject = () => {
+    let prop = function () {
+    };
+    prop.__proto__ = prop.prototype;
+    return prop;
+};
+const incrementor = () => {
+    if (!incrementor.counter) {
+        incrementor.counter = 0;
+    }
+    incrementor.valueOf = function () {
+        return incrementor.counter;
+    }
+    let incr = function () {
+        return incrementor();
+    }
+    incr.valueOf = function () {
+        return incrementor.counter;
+    }
+    incrementor.counter++;
+    return incr;
+};
+const asyncIncrementor = () => {
+    if (!asyncIncrementor.counter) {
+        asyncIncrementor.counter = 0;
+    }
+    asyncIncrementor.counter++;
+    return asyncIncrementor.counter;
+};
+const createIncrementer = () => {
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(param);
+        }, 1000);
+    })
+};
+
+const getDeepPropertiesCount = () => {
+};
+
+const createSerializedObject = () => {
+};
+const toBuffer = () => {
+};
+const sortByProto = (arr) => {
+    return arr.sort((a, b) => a - b);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
